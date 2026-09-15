7d3774e39b03ac95bede5f55c60d2ee18049829b
srivarshini438 <srivarshini438@gmail.com>
2026-09-15 11:29:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("theme-toggle");
+
+button.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+
+        button.textContent = "☀️ Light Mode";
+
+    } else {
+
+        button.textContent = "🌙 Dark Mode";
+
+    }
+
+});
